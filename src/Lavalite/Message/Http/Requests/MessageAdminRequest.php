751b9b0f0c85ce10707ffc2739a0a611da066fb9
George John <george@renfos.com>
2016-05-11 12:02:57 +0530

    Enabled multi auth for lavaliere 4.2

--- src/Lavalite/Message/Http/Requests/MessageAdminRequest.php
+++ src/Lavalite/Message/Http/Requests/MessageAdminRequest.php
@@ -3,7 +3,6 @@
 namespace Lavalite\Message\Http\Requests;
 
 use App\Http\Requests\Request;
-use Gate;
 
 class MessageAdminRequest extends Request
 {
@@ -15,28 +14,29 @@ class MessageAdminRequest extends Request
     public function authorize(\Illuminate\Http\Request $request)
     {
         $message = $this->route('message');
-        // Determine if the user is authorized to access message module,
+
         if (is_null($message)) {
-            return $request->user()->canDo('message.message.view');
+            // Determine if the user is authorized to access message module,
+            return $request->user('admin.web')->canDo('message.message.view');
         }
 
-        // Determine if the user is authorized to create an entry,
         if ($request->isMethod('POST') || $request->is('*/create')) {
-            return Gate::allows('create', $message);
+            // Determine if the user is authorized to create an entry,
+            return $request->user('admin.web')->can('create', $message);
         }
 
-        // Determine if the user is authorized to update an entry,
         if ($request->isMethod('PUT') || $request->isMethod('PATCH') || $request->is('*/edit')) {
-            return Gate::allows('update', $message);
+            // Determine if the user is authorized to update an entry,
+            return $request->user('admin.web')->can('update', $message);
         }
 
-        // Determine if the user is authorized to delete an entry,
         if ($request->isMethod('DELETE')) {
-            return Gate::allows('delete', $message);
+            // Determine if the user is authorized to delete an entry,
+            return $request->user('admin.web')->can('delete', $message);
         }
 
         // Determine if the user is authorized to view the module.
-        return Gate::allows('view', $message);
+        return $request->user('admin.web')->can('view', $message);
     }
 
     /**
@@ -46,15 +46,16 @@ class MessageAdminRequest extends Request
      */
     public function rules(\Illuminate\Http\Request $request)
     {
-        // validation rule for create request.
+
+// validation rule for create request.
         if ($request->isMethod('POST')) {
             return [
-                'to'    => 'required',
-              
+                'to' => 'required',
+
             ];
         }
 
-        // Validation rule for update request.
+// Validation rule for update request.
         if ($request->isMethod('PUT') || $request->isMethod('PATCH')) {
             return [
             ];
@@ -65,4 +66,5 @@ class MessageAdminRequest extends Request
 
         ];
     }
+
 }
